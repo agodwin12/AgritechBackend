47e4cc8d4ffe575558fe42e5644a1fe903d2795f
agodwin12 <wilbroadnde@gmail.com>
2025-05-16 11:28:13 +0100

    new

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser, googleLogin } = require('../controllers/loginController');
+const { loginUser, googleLogin, forgotPassword, resetPassword} = require('../controllers/loginController');
 
 router.post('/login', loginUser);
 router.post('/auth/google-login', googleLogin);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
+
+
 
 module.exports = router;
